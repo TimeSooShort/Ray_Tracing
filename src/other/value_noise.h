ae38569990b620eb872ddeecb9fb28aeb697b5b4
miaoquanwei <1160292058@qq.com>
2022-02-25 06:04:43 +0800

    value noise

--- src/other/value_noise.h
+++ src/other/value_noise.h
@@ -15,6 +15,11 @@
 你可以把它看作一个信号（如果你画出这个函数，你会得到一条曲线，这就是你的信号）,噪声函数可能是由多个频率组成的（低频意味着大范围的变化，高频意味着微小的变化）。
 */
 
+enum class ValueNaiseType
+{
+	WHITE_NOISE,VALUE_NOISE,FRACTAL_PATTERN,TURBULENCE_PATTERN,MARBLE_PATTERN,WOOD_PATTERN
+};
+
 inline double smoothstep(const double& t)
 {
 	//t* t* (3 - 2 * t);
@@ -79,6 +84,7 @@ double ValueNoise::eval(Vec3& p) const
 	int ry0 = yi & kMaxTableSizeMask;
 	int ry1 = (yi + 1) & kMaxTableSizeMask;
 
+	// grid 四个顶点的值
 	const double& c00 = r[permutationTable[permutationTable[rx0] + ry0]];
 	const double& c10 = r[permutationTable[permutationTable[rx1] + ry0]];
 	const double& c01 = r[permutationTable[permutationTable[rx0] + ry1]];
@@ -87,39 +93,145 @@ double ValueNoise::eval(Vec3& p) const
 	return bilinear<Vec3>(tx, ty, c00, c10, c01, c11);
 }
 
-void testValueNoise()
+void valueNoise(const ValueNaiseType& type)
 {
 	unsigned imageWidth = 512;
 	unsigned imageHeight = 512;
 	double* noiseMap = new double[imageWidth * imageHeight];
 
-#if 0
-	// 白噪音
-	for (int r = 0; r < imageHeight; ++r)
+	switch (type)
 	{
-		for (int c = 0; c < imageWidth; ++c)
+	case ValueNaiseType::WHITE_NOISE:
+		// 白噪音
+		for (unsigned r = 0; r < imageHeight; ++r)
 		{
-			noiseMap[r * imageWidth + c] = random_double();
+			for (unsigned c = 0; c < imageWidth; ++c)
+			{
+				noiseMap[r * imageWidth + c] = random_double();
+}
 		}
-	}
-#else
-	// value noise
-	ValueNoise noise;
-	double frequency = 0.05; // 频率
-	for (int r = 0; r < imageHeight; ++r)
-	{
-		for (int c = 0; c < imageWidth; ++c)
+		break;
+	case ValueNaiseType::VALUE_NOISE:
+		// value noise
+		ValueNoise noise;
+		double frequency = 0.05; // 频率
+		for (unsigned r = 0; r < imageHeight; ++r)
 		{
-			noiseMap[r * imageWidth + c] = noise.eval(Vec3{c, r, 0}*frequency);
+			for (unsigned c = 0; c < imageWidth; ++c)
+			{
+				noiseMap[r * imageWidth + c] = noise.eval(Vec3{ c, r, 0 }*frequency);
+			}
 		}
+		break;
+	case ValueNaiseType::FRACTAL_PATTERN:
+		// 分形
+		ValueNoise noise;
+		double frequency = 0.02; // 频率
+		// 通过改变间隙度和增益的值可以获得许多不同的外观
+		double frequencyMult = 1.8; // lacunarity 间隙度
+		double amplitudeMult = 0.35; // gain 增益
+		unsigned numLayers = 5; // 叠加个数
+		double maxNoiseVal = 0.0; // 叠加中可能大小超过一，记录最大的大小用于规范化
+		for (unsigned r = 0; r < imageHeight; ++r)
+		{
+			for (unsigned c = 0; c < imageWidth; ++c)
+			{
+				double amplitude = 1.0; // 振幅
+				Vec3 pNoise = Vec3(c, r, 0.0) * frequency;
+				// // 分形和可用于创建令人信服的地形和许多其他自然模式（海景、景观等）
+				for (unsigned l = 0; l < numLayers; ++l)
+				{
+					noiseMap[r * imageWidth + c] += noise.eval(pNoise) * amplitude;
+					pNoise *= frequencyMult;
+					amplitude *= amplitudeMult;
+				}
+				if (noiseMap[r * imageWidth + c] > maxNoiseVal)
+				{
+					maxNoiseVal = noiseMap[r * imageWidth + c];
+				}
+			}
+		}
+		for (unsigned i = 0; i < imageHeight * imageWidth; ++i) noiseMap[i] /= maxNoiseVal;
+		break;
+	case ValueNaiseType::TURBULENCE_PATTERN:
+		// 湍流
+		ValueNoise noise;
+		double frequency = 0.02; 
+		double frequencyMult = 1.8; 
+		double amplitudeMult = 0.35;
+		unsigned numLayers = 5; 
+		double maxNoiseVal = 0.0; 
+		for (unsigned r = 0; r < imageHeight; ++r)
+		{
+			for (unsigned c = 0; c < imageWidth; ++c)
+			{
+				double amplitude = 1.0;
+				Vec3 pNoise = Vec3(c, r, 0.0) * frequency;
+				for (unsigned l = 0; l < numLayers; ++l)
+				{
+					// 先使噪声函数返回值再[-1,1]之间，再将为负的部分取反，这样做的效果是
+					// 将创建一个似乎由凸起组成的轮廓,生成的曲线看起来凹凸不平
+					// 可以生成适合于模拟火、烟或云的图案
+					noiseMap[r * imageWidth + c] += std::fabs(2 * noise.eval(pNoise) - 1) * amplitude;
+					pNoise *= frequencyMult;
+					amplitude *= amplitudeMult;
+				}
+				if (noiseMap[r * imageWidth + c] > maxNoiseVal)
+				{
+					maxNoiseVal = noiseMap[r * imageWidth + c];
+				}
+			}
+		}
+		for (unsigned i = 0; i < imageHeight * imageWidth; ++i) noiseMap[i] /= maxNoiseVal;
+		break;
+	case ValueNaiseType::MARBLE_PATTERN:
+		// 大理石
+		ValueNoise noise;
+		double frequency = 0.02; 
+		double frequencyMult = 1.8; 
+		double amplitudeMult = 0.35; 
+		unsigned numLayers = 5;
+		for (unsigned r = 0; r < imageHeight; ++r)
+		{
+			for (unsigned c = 0; c < imageWidth; ++c)
+			{
+				double amplitude = 1.0;
+				Vec3 pNoise = Vec3(c, r, 0.0) * frequency;
+				double noiseValue = 0.0;
+				for (unsigned l = 0; l < numLayers; ++l)
+				{
+					noiseValue += noise.eval(pNoise) * amplitude;
+					pNoise *= frequencyMult;
+					amplitude *= amplitudeMult;
+				}
+				// 调制正弦图案的相位,扰动我们用来创建模式的函数
+				// 这种思想可以用来在任何周期性或规则性函数中引入随机性
+				noiseMap[r * imageWidth + c] = (sin((c + noiseValue * 100) * 2 * M_PI / 200.0) + 1) / 2.0;
+			}
+		}
+		break;
+	case ValueNaiseType::WOOD_PATTERN:
+		// 木质纹理
+		ValueNoise noise;
+		double frequency = 0.01;
+		for (unsigned r = 0; r < imageHeight; ++r)
+		{
+			for (unsigned c = 0; c < imageWidth; ++c)
+			{
+				// 噪声函数曲线会出现诸多断开(breakups)的情况，在2D中这些breakups就是亮颜色和深颜色区域之间的边界
+				// 从而产生木质纹理
+				double g = noise.eval(Vec3(c, r, 0.0) * frequency) * 10;
+				noiseMap[r * imageWidth + c] = g - static_cast<int>(g);
+			}
+		}
+		break;
 	}
-#endif
 	
 	std::ofstream ofs;
 	ofs.open("./noise.ppm", std::ios::out | std::ios::binary);
 	ofs << "P6\n" << imageWidth << " " << imageHeight << "\n255\n";
 	for (unsigned k = 0; k < imageWidth * imageHeight; ++k) {
-		unsigned char n = static_cast(noiseMap[k] * 255);
+		unsigned char n = static_cast<unsigned char>(noiseMap[k] * 255);
 		ofs << n << n << n;
 	}
 	ofs.close();
